86d8031d2df4e4f03cefb0d7b233d2b4bbbec53b
alex.oleshkevich <alex.oleshkevich@gmail.com>
2026-06-09 18:33:50 +0200

    Add App runner with server and curator

---
+++ dailydrift/app/app.go
@@ -0,0 +1,43 @@
+package app
+
+import (
+	"context"
+	"errors"
+
+	"github.com/starfrontventures/dailydrift/dailydrift/config"
+	"github.com/starfrontventures/dailydrift/dailydrift/curator"
+	"github.com/starfrontventures/dailydrift/dailydrift/server"
+	"golang.org/x/sync/errgroup"
+)
+
+type App struct {
+	cfg     *config.Config
+	server  *server.Server
+	curator *curator.Curator
+}
+
+func (a *App) Run(ctx context.Context) error {
+	g, ctx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		return a.server.Run(ctx)
+	})
+	g.Go(func() error {
+		return a.curator.Run(ctx)
+	})
+	if err := g.Wait(); err != nil && !errors.Is(err, context.Canceled) {
+		return err
+	}
+	return nil
+}
+
+func NewApp(cfg *config.Config) *App {
+	curator := curator.NewCurator()
+	server := server.NewServer(server.Deps{
+		Config: cfg,
+	})
+	return &App{
+		cfg:     cfg,
+		server:  server,
+		curator: curator,
+	}
+}
